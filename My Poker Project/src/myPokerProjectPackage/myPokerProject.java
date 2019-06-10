5e434bf011687bdfccc85216dbf39efd739a5149
Jamie <Jamie@MSI>
2019-06-10 15:31:11 +0100

    Prints value of cards in hand. Prints poker hand results

--- My Poker Project/src/myPokerProjectPackage/myPokerProject.java
+++ My Poker Project/src/myPokerProjectPackage/myPokerProject.java
@@ -13,6 +13,7 @@ public class myPokerProject {
 		for(int i=0; i<52; i++) myDeck.add(i);
 		Collections.shuffle(myDeck);
 		
+		
 		// Deal 5 cards from deck into hand
 		List<Integer> myHand = new ArrayList<Integer>();
 		for(int i=0; i<5; i++) {
@@ -20,14 +21,15 @@ public class myPokerProject {
 			myDeck.remove(0);
 		}
 		
+		// Print cards in hand, and result
 		printHand(myHand);
-		boolean[] myStraightFlushCheck = checkStraightFlush(myHand);
-		System.out.println(Arrays.toString(myStraightFlushCheck));
+		checkHand(myHand);
 
 	}
 	
+	// Prints value of myHand in text format
 	public static void printHand(List<Integer> myHand) {
-		String[] myValueArray = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+		String[] myValueArray = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
 		String[] mySuitArray = { "Diamonds", "Hearts", "Clubs", "Spades" };
 		int myValue, mySuit;
 		for(int i=0; i<5; i++) {
@@ -37,22 +39,65 @@ public class myPokerProject {
 		}
 	}
 	
-	public static boolean[] checkStraightFlush(List<Integer> myHand) {
-		int[] checkStraight = new int[5], checkFlush = new int[5];
+	
+	public static void checkHand(List<Integer> myHand) {
+		// 0=Pair, 1=Two Pair, 2=Three of a Kind, 3=Straight, 4=Flush, 5=Full House, 6=Four of a Kind, 7=Straight Flush
+		boolean[] myCheck = new boolean[8];
+		
+		
+		// Split cards into Values and Suits
+		int[] myValues = new int[5], mySuits = new int[5];
 		for(int i=0; i<5; i++) {
-			checkStraight[i] = myHand.get(i) % 13;
-			checkFlush[i] = (int) Math.floor(myHand.get(i)/13);
+			myValues[i] = myHand.get(i) % 13;
+			mySuits[i] = (int) Math.floor(myHand.get(i)/13);
 		}
-		Arrays.sort(checkStraight);
+		Arrays.sort(myValues);
+		
+		
+		// Check for Pair, Two Pair, Three of a Kind, Full House, Four of a Kind
+		int pairValue1 = 13, pairValue2 = 13;
+		for(int i=0; i<4; i++) {
+			if(myValues[i] == myValues[i+1]) {
+				if(!myCheck[0]) {
+					myCheck[0] = true;
+					pairValue1 = i;
+				} else {
+					if(myValues[i]!=pairValue1) {
+						myCheck[1] = true;
+						pairValue2 = i;
+					}
+					else if((myValues[i]==pairValue1)||(myValues[i]==pairValue2)) {
+						if(!myCheck[2]) myCheck[2] = true;
+						else if(!myCheck[6]) myCheck[6] = true;
+					}
+				}
+			}
+		}
+		if((pairValue1 != 13)&&(pairValue2 != 13)&&(myCheck[2])) myCheck[5] = true;
+		
+		
+		// Check for Straight, Flush, Straight Flush
 		int countStraight = 0, countFlush = 0;
 		for(int i=0; i<4; i++) {
-			if(checkStraight[i]+1 == checkStraight[i+1]) countStraight++;
-			if(checkFlush[i] == checkFlush[i+1]) countFlush++;
+			if(myValues[i]+1 == myValues[i+1]) countStraight++;
+			if(mySuits[i] == mySuits[i+1]) countFlush++;
 		}
-		boolean[] myReturn = new boolean[2];
-		if(countStraight == 4) myReturn[0] = true;
-		if(countFlush == 4) myReturn[1] = true;
-		return(myReturn);
+		if(countStraight == 4) myCheck[3] = true;
+		if(countFlush == 4) myCheck[4] = true;
+		if((myCheck[3])&&(myCheck[4])) myCheck[7] = true;
+		
+		
+		// Checks boolean for highest value
+		int myBestHand = 8;
+		for(int i=0; i<7; i++) {
+			if(myCheck[i]) myBestHand = i;
+		}
+		String myAnswer;
+		if(myBestHand!=8) {
+			String[] pokerHands = {"A Pair", "Two Pairs", "Three of a Kind", "A Straight", "A Flush", "A Full House", "Four of a Kind", "A Straight Flush"};
+			myAnswer = "You Have " + pokerHands[myBestHand] + "!";
+		} else myAnswer = "You Have Nothing! Good Day Sir!";
+				
+		System.out.println(myAnswer);
 	}
-
 }
